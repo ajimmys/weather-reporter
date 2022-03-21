d45c0bf3fdd20d0f941adc3c00e95b188201c0f6
James Schnitzer <67064537+ajimmys@users.noreply.github.com>
2022-03-21 15:21:51 -0500

    Added Buttons for Navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import day from './images/day_image.png'
+import five_day from './images/five_day_image.png'
+import seven_day from './images/seven_day_image.png'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='controls'>
+          <button className='circle'>
+              <img src={day} alt="Day Image"/>
+          </button>
+        <button className='circle'>
+            <img src={five_day} alt="5 Day Image"/>
+        </button>
+        <button className='circle'>
+            <img src={seven_day} alt="7 Day Image"/>
+        </button>
+      </div>
+        <p className="attributions">Navigation Icons courtesy of <a href="https://www.flaticon.com/authors/pixel-prover">pixel prover</a> via Flaticon</p>
     </div>
   );
 }
